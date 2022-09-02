2289fe911406529a0edb9e9fef2c89958d10b984
JLumsden <jarrettlumsden@gmail.com>
2022-09-01 20:14:43 -0500

    Made bean

--- src/main/java/com/example/playlist_builder/repository/SetlistFmApiRepository.java
+++ src/main/java/com/example/playlist_builder/repository/SetlistFmApiRepository.java
@@ -5,9 +5,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 
 @AllArgsConstructor
+@Repository
 public class SetlistFmApiRepository {
     private final RestTemplate restTemplate = new RestTemplate();
 
