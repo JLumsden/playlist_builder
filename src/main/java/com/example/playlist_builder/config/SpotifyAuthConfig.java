9407b3118457f7f93b510753b2bcef27bb03caf7
JLumsden <jarrettlumsden@gmail.com>
2022-09-03 21:57:27 -0500

    Added for cleaner code

---
+++ src/main/java/com/example/playlist_builder/config/SpotifyAuthConfig.java
@@ -0,0 +1,15 @@
+package com.example.playlist_builder.config;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@NoArgsConstructor
+@Data
+public class SpotifyAuthConfig {
+    private final String clientId = "";
+    private final String redirectUrl = "http://localhost:8080/redirect";
+    private final String authUrl = "https://accounts.spotify.com/api/token";
+    private final String authScopes = "playlist-read-collaborative,playlist-modify-public,playlist-read-private,playlist-modify-private";
+}
